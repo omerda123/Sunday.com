3fa3cf4986ca2eff2735b1accf1a035770afbbb0
Omer Daniel <omerda@gmail.com>
2020-01-10 08:36:09 +0200

    try to publish 2

---
+++ public/precache-manifest.69f86795f05f9988b3cd63abd4831bf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276ea9d4c65b1864affced45e4295565",
+    "url": "/Sundy.com/public/index.html"
+  },
+  {
+    "revision": "f8b5c1af76a6f0c1c7fb",
+    "url": "/Sundy.com/public/static/css/main.e6d25d8a.chunk.css"
+  },
+  {
+    "revision": "c2f583cc41cc3b645153",
+    "url": "/Sundy.com/public/static/js/2.6fb76bcf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Sundy.com/public/static/js/2.6fb76bcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8b5c1af76a6f0c1c7fb",
+    "url": "/Sundy.com/public/static/js/main.bf6d609a.chunk.js"
+  },
+  {
+    "revision": "2377933ddce3a4d76339",
+    "url": "/Sundy.com/public/static/js/runtime-main.37302ac5.js"
+  }
+]);
